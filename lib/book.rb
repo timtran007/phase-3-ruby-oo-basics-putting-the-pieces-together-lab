e1dd2473f3cc9ce46332474fe72aae76dea8a605
Tim <timtran007@gmail.com>
2022-11-18 14:48:18 -0600

    commit tree 7d18da593292aa731439eb29444638666223022e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+require "pry"
+
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize (title)
+        @title = title
+    end
+
+    def turn_page
+        if @title
+       puts "Flipping the page...wow, you read fast!"
+        else 
+            puts ""
+        end
+    end    
+end 
+
+lord_of_the_rings = Book.new(
+    "Fellowship of the Ring"
+)
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+require "pry"
+
+class Shoe
+    attr_accessor :brand,  :color, :size, :material, :condition
+
+    def initialize (brand)
+        @brand = brand
+    end
+
+    def cobble 
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
+airforce_95 = Shoe.new("Nike")
+
+
+binding.pry
